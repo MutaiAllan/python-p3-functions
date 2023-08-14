5109a90e86b033acc675e2f450fda87da9c5f2be
MutaiAllan <MutaiAllan>
2023-08-14 21:06:45 +0300

    Created corresponding Js functions in python

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print('Hello, programmer!')
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return 'null'
+    
+    return number / 2
+    
+
+# Calling the functions.
+greet_programmer()
+greet("Naureen")
+greet_with_default()
+greet_with_default('Sunny')
+print(add(1, 2))
+print(halve(4))
+print(halve("two"))
+
